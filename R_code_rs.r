64adae57997fbea824f9accb1e1fb0604085ee59
Giovanni <73804318+giovanninocera@users.noreply.github.com>
2022-09-28 18:57:59 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -5,3 +5,13 @@ library(raster)
 
 # Settaggio cartella di lavoro
 setwd("C:/lab/")
+
+l2011 <- brick("p224r63_2011.grd")
+l2011
+
+plot(l2011)
+
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(l2011, col=cl)
+
+
