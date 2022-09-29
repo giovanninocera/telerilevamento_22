4cb3395de1f1468429a2b97007bb3aba651fdd12
Giovanni <73804318+giovanninocera@users.noreply.github.com>
2022-09-29 18:32:39 +0200

    Create R_code_spectral_indices.r

---
+++ R_code_spectral_indices.r
@@ -0,0 +1,9 @@
+# Setting the working directory
+setwd("C:/lab/")
+
+# install.packages("raster")
+# install.packages("RStoolbox")
+
+# Recalling the library in order to use raster package funtions 
+library(raster)
+
