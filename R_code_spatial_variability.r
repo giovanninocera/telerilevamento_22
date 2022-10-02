921bbd425d9032ce55ba7178b1c670f4d00c3eff
Giovanni <73804318+giovanninocera@users.noreply.github.com>
2022-10-02 22:00:19 +0200

    Update and rename R_code_spatial_vasxscacdas.r to R_code_spatial_variability.r

---
+++ R_code_spatial_variability.r
@@ -0,0 +1,10 @@
+# Spatial variability Similaun glacier
+library(raster)
+library(RStoolbox)
+library(ggplot2)
+library(patchwork)
+# install.packages("viridis")
+library(viridis)
+
+setwd("C:/lab/")
+
--- R_code_spatial_vasxscacdas.r
+++
@@ -1,6 +0,0 @@
-sscasxA
-
-
-CSA
-
-ss
