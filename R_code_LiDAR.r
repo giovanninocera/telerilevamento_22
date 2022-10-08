7b6095623bd2e1235b7e28cab40748ab12da549c
Giovanni <73804318+giovanninocera@users.noreply.github.com>
2022-10-08 14:48:33 +0200

    Create R_code_LiDAR.r

---
+++ R_code_LiDAR.r
@@ -0,0 +1,13 @@
+# R code for visualising and analysing LiDAR data
+library(raster)
+library(RStoolbox) # for image viewing and variability
+library(ggplot2) # for ggplot plotting
+library(patchwork)
+library(gridExtra) # for plotting ggplots together
+library(viridis) # for ggplots colouring
+
+setwd("C:/lab/")
+
+
+
+
